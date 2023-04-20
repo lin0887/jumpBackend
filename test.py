1d58dfec451bc252d312fcfbc25ada5fb5700265
lin0887 <ww70611@gmail.com>
2023-04-21 02:14:25 +0800

    feat: 增加將報名表轉換成 json 的程式

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
-import requests
+import pandas as pd
+import re
 
-url = 'http://120.125.89.209:8000/uploadVideo/A1111'
+# Load Excel file
+excel_file = pd.ExcelFile('112跳繩各校名單.xlsx')
 
-with open('./009.mp4', 'rb') as f:
-    r = requests.post(url, files={'video': f})
-    
-print(r.status_code)
+# Loop through all sheets and read data into DataFrame
+all_df = pd.DataFrame(columns=['項目', '組別', '年級', '學校', '姓名', '指導老師', 'ID'])
+for sheet_name in excel_file.sheet_names:
+    df = excel_file.parse(sheet_name)
+    #print(df.columns)
+    tmp = df.columns
+    df.columns = ['項目', '組別', '年級', '學校', '姓名', '指導老師', 'ID']
+    df.loc[len(df), :] = tmp
+    all_df = all_df.append(df)
+
+all_df = all_df.sort_values(by=['ID'])
+
+all_df.to_json('contestants.json', orient='records')
+#all_df.to_excel('t.xlsx',index=False)
