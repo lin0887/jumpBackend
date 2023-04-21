87c5d3a0a5f58caaddf8735340103e3b01cb07c0
lin0887 <ww70611@gmail.com>
2023-04-22 01:25:31 +0800

    style: 修改寫入格式

--- getRegistrationForm.py
+++ getRegistrationForm.py
@@ -1,25 +1,20 @@
 import pandas as pd
-import numpy as np
 import re
 
-# Load Excel file
-excel_file = pd.ExcelFile('..\\名單\\112跳繩各校名單.xlsx')
+# 讀取Excel檔案
+excel_file = pd.ExcelFile("112v.xlsx")
 
-col = ['項目', '組別', '年級', '學校', '姓名', '指導老師', 'ID']
-# Loop through all sheets and read data into DataFrame
-all_df = pd.DataFrame(columns = col)
+
+# read all sheets
+all_df = pd.DataFrame()
 for sheet_name in excel_file.sheet_names:
     df = excel_file.parse(sheet_name)
-    tmp = df.columns
-    df.columns = col
-    df.loc[len(df), :] = tmp
-    
-    all_df = all_df.append(df)
+    sheet_df = pd.read_excel("112v.xlsx", sheet_name=sheet_name, header=None)
+    all_df = all_df.append(sheet_df, ignore_index=True)
 
-all_df['成績'] = np.zeros(len(all_df), dtype=int)
-all_df['等第'] = np.full(len(all_df),np.nan)
-  
-all_df = all_df.sort_values(by=['ID'])
-
-all_df.to_json('contestants.json', orient='records')
+# 加上欄位名稱
+all_df.columns = ["項目", "組別", "年級", "學校", "選手", "教練", "編號"]
+all_df.rename (columns={"項目": "contest", "組別": "group", "年級": "grade", "學校": "school", "選手": "name", "教練": "teacher", "編號": "id"},inplace=True)
+all_df.dropna(subset=["id"], inplace=True)
+all_df.to_json("contestants.json", orient="records")
 
