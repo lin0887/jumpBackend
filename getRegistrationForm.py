1f7bd9103f45b4da1c8552df2ecb69e142086737
lin0887 <ww70611@gmail.com>
2023-04-22 01:50:50 +0800

    style: 修改column names

--- getRegistrationForm.py
+++ getRegistrationForm.py
@@ -1,20 +1,22 @@
 import pandas as pd
+import numpy as np
 import re
 
 # 讀取Excel檔案
-excel_file = pd.ExcelFile("112v.xlsx")
+excel_file = pd.ExcelFile("..\\名單\\112跳繩各校名單.xlsx")
 
 
 # read all sheets
 all_df = pd.DataFrame()
 for sheet_name in excel_file.sheet_names:
     df = excel_file.parse(sheet_name)
-    sheet_df = pd.read_excel("112v.xlsx", sheet_name=sheet_name, header=None)
+    sheet_df = pd.read_excel("..\\名單\\112跳繩各校名單.xlsx", sheet_name=sheet_name, header=None)
     all_df = all_df.append(sheet_df, ignore_index=True)
 
 # 加上欄位名稱
 all_df.columns = ["項目", "組別", "年級", "學校", "選手", "教練", "編號"]
-all_df.rename (columns={"項目": "contest", "組別": "group", "年級": "grade", "學校": "school", "選手": "name", "教練": "teacher", "編號": "id"},inplace=True)
+all_df['成績'] =  np.zeros(len(all_df), dtype=int)
+all_df.rename (columns={"項目": "contest", "組別": "group", "年級": "grade", "學校": "school", "選手": "name", "教練": "teacher", "編號": "id", "成績": "score"},inplace=True)
 all_df.dropna(subset=["id"], inplace=True)
 all_df.to_json("contestants.json", orient="records")
 
