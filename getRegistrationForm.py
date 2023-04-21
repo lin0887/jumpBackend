3d28e4c40f2fc8439e3a1db52faea4ea1aab4368
lin <ww70611@gmail.com>
2023-04-21 23:34:20 +0800

    style: 變更輸出excel的格式

--- getRegistrationForm.py
+++ getRegistrationForm.py
@@ -3,7 +3,7 @@ import numpy as np
 import re
 
 # Load Excel file
-excel_file = pd.ExcelFile('112跳繩各校名單.xlsx')
+excel_file = pd.ExcelFile('..\\名單\\112跳繩各校名單.xlsx')
 
 col = ['項目', '組別', '年級', '學校', '姓名', '指導老師', 'ID']
 # Loop through all sheets and read data into DataFrame
@@ -17,8 +17,9 @@ for sheet_name in excel_file.sheet_names:
     all_df = all_df.append(df)
 
 all_df['成績'] = np.zeros(len(all_df), dtype=int)
+all_df['等第'] = np.full(len(all_df),np.nan)
   
 all_df = all_df.sort_values(by=['ID'])
 
 all_df.to_json('contestants.json', orient='records')
-all_df.to_excel('112跳繩各校成績.xlsx',index=False)
+
